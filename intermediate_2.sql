672375a2e71c9bc0cded93df57d63f9a4806fb2a
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-02-09 13:51:52 -0600

    add ntile func

--- intermediate_2.sql
+++ intermediate_2.sql
@@ -30,3 +30,36 @@ SELECT p.*, AVG(p.amount) OVER w
 FROM payment p
 WINDOW w AS (ORDER BY p.payment_id ROWS BETWEEN 0 PRECEDING AND 0 FOLLOWING);
 -- 0 and 0 returns the same amount as amount, replacing 0 with values will calculate a moving avg
+
+-- NTILE Window Function
+
+-- top 10% of movies by dollar value rented
+
+-- without window function
+WITH movie_sales AS (
+	SELECT f.film_id, f.title, SUM(p.amount) as sales
+	FROM film f
+	JOIN inventory i USING(film_id)
+	JOIN rental r USING(inventory_id)
+	JOIN payment p USING(rental_id)
+	GROUP BY 1, 2
+	ORDER BY 3 DESC
+)
+SELECT *
+FROM movie_sales
+LIMIT cast((SELECT COUNT(*) FROM movie_sales) * 0.1 AS INT);
+-- returns 96 rows
+
+-- with window function
+WITH movie_sales AS (
+	SELECT f.film_id, f.title, SUM(p.amount) as sales,
+			NTILE(100) OVER(ORDER BY SUM(p.amount) DESC) AS percentile_rank,
+			SUM(SUM(p.amount)) OVER() AS gloabal_sales
+	FROM film f
+	JOIN inventory i USING(film_id)
+	JOIN rental r USING(inventory_id)
+	JOIN payment p USING(rental_id)
+	GROUP BY 1, 2
+)
+SELECT * FROM movie_sales WHERE percentile_rank <= 10;
+-- returns 100 rows
